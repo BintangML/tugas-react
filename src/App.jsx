3420665eeafb3066df296dc6851b6a4dd3c2f6f8
BintangML <bintang040710@student.abudzar.sch.id>
2026-02-06 09:32:22 +0700

    blablabla

--- src/App.jsx
+++ src/App.jsx
@@ -1,120 +1,120 @@
-// import Layout from "./layot/Layot";
-// import AlertButton from "./AlertButton/AlertButton.jsx";
-
-// function App() {
-//   return (
-//     <Layout>
-//       <h1>Welcome to My React App</h1>
-//       <p>keren kan😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏</p>
-//       <AlertButton 
-//         text="Coba aje" 
-//         message="kece banget kan buatan ane😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏"
-//       />
-//     </Layout>
-//   );
-// }
-
-// export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// // import Layout from "./layot/Layot";
+// // import AlertButton from "./AlertButton/AlertButton.jsx";
 
 // // function App() {
-// //   const [count, setCount] = useState(0)
-
 // //   return (
-// //     <>
-// //       <div>
-// //         <a href="https://vite.dev" target="_blank">
-// //           <img src={viteLogo} className="logo" alt="Vite logo" />
-// //         </a>
-// //         <a href="https://react.dev" target="_blank">
-// //           <img src={reactLogo} className="logo react" alt="React logo" />
-// //         </a>
-// //       </div>
-// //       <h1>Vite + React</h1>
-// //       <div className="card">
-// //         <button onClick={() => setCount((count) => count + 1)}>
-// //           count is {count}
-// //         </button>
-// //         <p>
-// //           Edit <code>src/App.jsx</code> and save to test HMR
-// //         </p>
-// //       </div>
-// //       <p className="read-the-docs">
-// //         Click on the Vite and React logos to learn more
-// //       </p>
-// //     </>
-// //   )
+// //     <Layout>
+// //       <h1>Welcome to My React App</h1>
+// //       <p>keren kan😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏</p>
+// //       <AlertButton 
+// //         text="Coba aje" 
+// //         message="kece banget kan buatan ane😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏"
+// //       />
+// //     </Layout>
+// //   );
 // // }
 
-// // export default App
+// // export default App;
 
-import AlertButton from "./AlertButton/AlertButton.jsx";
 
-export default function App() {
-  return (
-    <div>
-      <AlertButton text="Klik Disini" message="Berhasil diubah!" />
-    </div>
-  );
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // // function App() {
+// // //   const [count, setCount] = useState(0)
+
+// // //   return (
+// // //     <>
+// // //       <div>
+// // //         <a href="https://vite.dev" target="_blank">
+// // //           <img src={viteLogo} className="logo" alt="Vite logo" />
+// // //         </a>
+// // //         <a href="https://react.dev" target="_blank">
+// // //           <img src={reactLogo} className="logo react" alt="React logo" />
+// // //         </a>
+// // //       </div>
+// // //       <h1>Vite + React</h1>
+// // //       <div className="card">
+// // //         <button onClick={() => setCount((count) => count + 1)}>
+// // //           count is {count}
+// // //         </button>
+// // //         <p>
+// // //           Edit <code>src/App.jsx</code> and save to test HMR
+// // //         </p>
+// // //       </div>
+// // //       <p className="read-the-docs">
+// // //         Click on the Vite and React logos to learn more
+// // //       </p>
+// // //     </>
+// // //   )
+// // // }
+
+// // // export default App
+
+// import AlertButton from "./AlertButton/AlertButton.jsx";
+
+// export default function App() {
+//   return (
+//     <div>
+//       <AlertButton text="Klik Disini" message="Berhasil diubah!" />
+//     </div>
+//   );
+// }
